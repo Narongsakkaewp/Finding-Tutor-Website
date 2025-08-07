70ff04c00bb112d26c7e252ba77cb25d6690d654
Narongsakkaewp <152575175+Narongsakkaewp@users.noreply.github.com>
2025-08-07 14:08:45 +0700

    update navbar.jsx

--- tutorweb/src/App.js
+++ tutorweb/src/App.js
@@ -1,11 +1,10 @@
+import React from "react";
+import Navbar from "./components/navbar";
+
 function App() {
   return (
     <div>
-      <h1>Finding Tutor Website</h1>
-      <p>Welcome to the Finding Tutor website!</p>
-      <div className="text-3xl font-bold underline text-green-500">
-        Hello Tailwind!
-      </div>
+      <Navbar />
     </div>
   );
 }
--- tutorweb/tailwind.config.js
+++ tutorweb/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Sarabun', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
