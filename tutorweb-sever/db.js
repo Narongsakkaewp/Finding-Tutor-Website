953c110b746a483c51d109547ec5b00bf3004cd7
Narongsakkaewp <152575175+Narongsakkaewp@users.noreply.github.com>
2026-02-25 16:57:25 +0700

    fix: update node version and package-lock

--- tutorweb-sever/db.js
+++ tutorweb-sever/db.js
@@ -3,10 +3,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '127.0.0.1',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'tutorweb',
+    database: process.env.DB_NAME || 'tutorweb_bckup',
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
